636ce8d360de73ac62d8c5ca1016a2c14a39d821
erkan esen <erkanesen2202@hotmail.com>
2018-07-07 23:23:32 +0300

    Add project files.

---
+++ WindowsApplication1/Form1.Designer.cs
@@ -0,0 +1,328 @@
+namespace WindowsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oyunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.baþlaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayarlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oyunToolStripMenuItem,
+            this.ayarlarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(827, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oyunToolStripMenuItem
+            // 
+            this.oyunToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.baþlaToolStripMenuItem});
+            this.oyunToolStripMenuItem.Name = "oyunToolStripMenuItem";
+            this.oyunToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.oyunToolStripMenuItem.Text = "Oyun";
+            // 
+            // baþlaToolStripMenuItem
+            // 
+            this.baþlaToolStripMenuItem.Name = "baþlaToolStripMenuItem";
+            this.baþlaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.baþlaToolStripMenuItem.Text = "Baþla";
+            this.baþlaToolStripMenuItem.Click += new System.EventHandler(this.baþlaToolStripMenuItem_Click);
+            // 
+            // ayarlarToolStripMenuItem
+            // 
+            this.ayarlarToolStripMenuItem.Name = "ayarlarToolStripMenuItem";
+            this.ayarlarToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.ayarlarToolStripMenuItem.Text = "Hakkýnda";
+            this.ayarlarToolStripMenuItem.Click += new System.EventHandler(this.ayarlarToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(659, 249);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(149, 379);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgi";
+            this.groupBox1.Visible = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(28, 344);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(36, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(28, 304);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(36, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(25, 229);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(39, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(25, 190);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(39, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 327);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(12, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "y";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "y";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Çift Click Koordinat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Click Koordinat";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 288);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(12, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mouse Koordinat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "x";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "x";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Gray;
+            this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Location = new System.Drawing.Point(659, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(168, 216);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Skorlar";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(54, 43);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(40, 20);
+            this.textBox7.TabIndex = 3;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(21, 27);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(107, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Toplam Eklenen Sayý";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 609);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "VEYS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oyunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem baþlaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayarlarToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ WindowsApplication1/Form1.cs
@@ -0,0 +1,255 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Int32[,] oyun=new Int32[10,10];
+        Int32[,] kont = new Int32[10, 10];
+        int o, o1, son=1,skor=0,sag=1,sol=1,ust=1,alt=1,ustsol=1,ustsag=1,altsol=1,altsag=1;
+        Int32 x=1, y=1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            basla();
+
+        }
+
+        private void basla()
+        {
+            for (o = 0; o <= 9; o++)
+            {
+                for (o1 = 0; o1 <= 9; o1++)
+                {
+                    oyun[o, o1] = 0;
+                    kont[o, o1] = 0;
+                }
+            }
+            textBox7.Text = "1";
+            oyun[0, 0] = 1;
+            skor = 0;
+            son = 2;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            int i;
+            SolidBrush br = new SolidBrush(Color.LightGreen);
+            SolidBrush brush = new SolidBrush(Color.DarkBlue);
+            FontStyle style = FontStyle.Bold;
+            Font arial = new Font("Arial", 16, style);
+            Pen P=new Pen(Color.Black,2);
+            e.Graphics.TranslateTransform(40, 40);
+            e.Graphics.DrawRectangle(P, 0, 0, 500, 500);
+            for (i = 1; i <= 9;i++ )
+            {
+                e.Graphics.DrawLine(P, 0, 50*i, 500, 50*i);
+                e.Graphics.DrawLine(P, 50*i,0, 50*i, 500);
+            }
+            //rakam koy
+            //sola iþaret koy
+            if ((x - 3) >= 1)
+            {
+                if (oyun[x - 4, y - 1] == 0)
+                {
+                    e.Graphics.FillRectangle(br, (x * 50) - (200), (y - 1) * 50, 50, 50);
+                    sol = 1;
+                }else  {  sol = 0;  }
+            }else  {  sol = 0;  }
+
+            //saða iþaret koy
+            if ((x + 3) <= 10)
+            {
+                if (oyun[x + 2, y - 1] == 0)
+                {
+                    e.Graphics.FillRectangle(br, (x * 50) + (100), (y - 1) * 50, 50, 50);
+                    sag = 1;
+                }else  {  sag = 0;  }
+            }else  {  sag = 0;  }
+
+            //üst iþaret koy
+            if ((y - 3) >= 1)
+            {
+                if (oyun[x - 1, y - 4] == 0)
+                {
+                    e.Graphics.FillRectangle(br, (x - 1) * 50, (y * 50) - 200, 50, 50);
+                    ust = 1;
+                }else  {  ust = 0;  }
+            }else  {  ust = 0;  }
+
+            //alt iþaret koy
+            if ((y + 3) <= 10)
+            {
+                if (oyun[x - 1, y + 2] == 0)
+                {
+                    e.Graphics.FillRectangle(br, (x - 1) * 50, (y * 50) + 100, 50, 50);
+                    alt = 1;
+                }else  {  alt = 0;  }
+            }else  {  alt = 0;  }
+
+           //ust sol iþaret koy
+            if ((x - 2) >= 1 && (y - 2) >= 1)
+            {
+                if (oyun[x - 3, y - 3] == 0)
+                {
+                    e.Graphics.FillRectangle(br, (x * 50) - 150, (y *50) -150, 50, 50);
+                    ustsol = 1;
+                }
+                else { ustsol = 0; }
+            }
+            else { ustsol = 0; }
+
+           //ust sag iþaret koy
+            if ((x + 2) <= 10 && (y - 2) >= 1)
+            {
+                if (oyun[x + 1, y - 3] == 0)
+                {
+                    e.Graphics.FillRectangle(br, (x * 50) + 50, (y * 50) - 150, 50, 50);
+                    ustsag= 1;
+                }
+                else { ustsag = 0; }
+            }
+            else { ustsag = 0; }
+
+            //alt sol iþaret koy
+            if ((x - 2) >= 1 && (y + 2) <= 10)
+            {
+                if (oyun[x - 3, y + 1] == 0)
+                {
+                    e.Graphics.FillRectangle(br, (x * 50) - 150, (y * 50) + 50, 50, 50);
+                    altsol = 1;
+                }
+                else { altsol = 0; }
+            }
+            else { altsol= 0; }
+
+            //alt sað iþaret koy
+            if ((x + 2) <= 10 && (y + 2) <= 10)
+            {
+                if (oyun[x + 1, y + 1] == 0)
+                {
+                    e.Graphics.FillRectangle(br, (x * 50) + 50, (y * 50) + 50, 50, 50);
+                    altsag = 1;
+                }
+                else { altsag = 0; }
+            }
+            else { altsag = 0; }
+
+
+            //bitiþ mesajý
+            if (sag == 0 && sol == 0 && ust == 0 && alt == 0 && ustsol==0 && ustsag==0 && altsag==0 && altsol==0) 
+            {
+                MessageBox.Show("Oyun Bitti. Nereye Týklýcan Þimdi Skorun: "+ Convert.ToString(son-1)+" ok.:))");
+            } 
+
+            //rakamlarý yerleþtir
+            for (o = 0; o <= 9; o++)
+            {
+                for (o1 = 0; o1 <= 9; o1++)
+                {
+                    if (oyun[o, o1] != 0)
+                    {
+                        e.Graphics.DrawString(Convert.ToString(oyun[o, o1]), arial, brush, ((o) * 50) + 15, ((o1) * 50) + 15);
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+           //kural 1: sadece yeþillere týklasýn
+           //yeþil yollar
+                //üst
+                yesilYol(e, x - 1, x, y - 4, y - 3);
+                //alt
+                yesilYol(e, x - 1, x, y + 2, y + 3);
+                //sag
+                yesilYol(e, x + 2, x+3, y -1, y );
+                //sol
+                yesilYol(e, x - 4, x-3, y -1, y);
+                //üstsol
+                yesilYol(e, x - 3, x-2, y - 3, y - 2);
+                //üstsað
+                yesilYol(e, x +1, x+2, y - 3, y - 2);
+                //altsol
+                yesilYol(e, x - 3, x-2, y + 1, y + 2);
+                //altsag
+                yesilYol(e, x +1, x+2, y + 1, y + 2);
+        }
+
+        private void yesilYol(MouseEventArgs e, Int32 x1, Int32 x2, Int32 y1, Int32 y2)
+        {
+            if ((Convert.ToDouble(e.X) >= 40) && (Convert.ToDouble(e.Y) >= 40) && (Convert.ToDouble(e.X) <= 539) && (Convert.ToDouble(e.Y) <= 539))
+            {
+
+                if ((e.X > ((x1) * 50)+40) && (e.X < ((x2) * 50)+40) && (e.Y > ((y1) * 50)+40) && (e.Y < ((y2) * 50)+40))
+                {
+                    textBox3.Text = Convert.ToString(((e.X - 40) / 50) + 1);
+                    textBox4.Text = Convert.ToString(((e.Y - 40) / 50) + 1);
+                    x = Convert.ToInt32(textBox3.Text);
+                    y = Convert.ToInt32(textBox4.Text);
+                    
+                    //kural 2: ayný kareye bir daha týklamasýn
+                    if (oyun[x - 1, y - 1] == 0)
+                    {
+                        oyun[Convert.ToInt32(textBox3.Text) - 1, Convert.ToInt32(textBox4.Text) - 1] = son;
+                        son++;
+                    }
+                    skor = 0;
+                    for (o = 0; o <= 9; o++)
+                    {
+                        for (o1 = 0; o1 <= 9; o1++)
+                        {
+                            if (oyun[o, o1] > 0) skor++;
+                        }
+                    }
+                    textBox7.Text = Convert.ToString(skor);
+                    Invalidate();
+                }
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //this.Text = "x: "+Convert.ToString(e.X)+" y: "+Convert.ToString( e.Y);
+            textBox1.Text = Convert.ToString(e.X);
+            textBox2.Text = Convert.ToString(e.Y);
+            
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if ((Convert.ToDouble(e.X) >= 40) && (Convert.ToDouble(e.Y) >= 40) && (Convert.ToDouble(e.X) <= 539) && (Convert.ToDouble(e.Y) <= 539))
+            {
+                textBox5.Text = Convert.ToString(((e.X - 40) / 50) + 1);
+                textBox6.Text = Convert.ToString(((e.Y - 40) / 50) + 1);
+            }
+        }
+
+        private void baþlaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            basla();
+            x = 1;
+            y = 1;
+            Invalidate();
+        }
+
+        private void ayarlarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programlama: Erkan ESEN");
+        }
+    }
+}
